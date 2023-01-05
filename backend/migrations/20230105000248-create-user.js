35d51a2d95491b46bc1d9aa032ab332f1998cb26
steffao <steffie.jeanjoseph@gmail.com>
2023-01-05 13:41:20 +1300

    init sequelize + create sqlite db + create user model

---
+++ backend/migrations/20230105000248-create-user.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_name: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      is_admin: {
+        allowNull: false,
+      type: Sequelize.BOOLEAN,    
+      defaultValue: 0
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
